829cb265ac5e9b842f05c49b8221924eafb219ca
Jesmeen kaur <jesmeen@Jesmeens-MacBook-Air.local>
2019-03-13 16:25:49 -0700

    initial server

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+app.set('view engine', 'ejs');
+app.listen(3000);
+app.get('/', function(req, res) {
+    res.render('home');
+  });
+  app.get('*', function(req, res) {
+    res.render('error');
+  });
